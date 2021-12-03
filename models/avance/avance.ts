827b32cbe07db36caf8385da213017a330bc396e
narchila <90266760+narchila@users.noreply.github.com>
2021-12-02 20:10:44 -0500

    Se adiciona crear y actualizar avances

---
+++ models/avance/avance.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import { ProjectModel } from '../proyecto/proyecto';
+import { UseModel } from '../usuario/usuario';
+
+const { Schema, model } = mongoose;
+
+interface Avance {
+    fecha: Date;
+    descripcion: string;
+    observaciones: [string];
+    proyecto: Schema.Types.ObjectId;
+    creadoPor: Schema.Types.ObjectId;
+}
+
+const avanceSchema = new Schema({
+    fecha: {
+        type: Date,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    observaciones: [
+        {
+          type: String,
+        },
+    ],
+    proyecto: {
+        type: Schema.Types.ObjectId,
+        ref: ProjectModel,
+        required: true,
+    },
+    creadoPor: {
+        type: Schema.Types.ObjectId,
+        ref: UserModel,
+        required: true,
+    },
+});
+
+
+const ModeloAvance = model('Avance', avanceSchema);
+
+export { ModeloAvance };
---
+++ models/avance/resolvers.ts
@@ -0,0 +1,30 @@
+import { ModeloAvance } from './avance';
+
+const resolversAvance = {
+    Query:{
+        Avances: async (parent, args) => {
+            const avances = await ModeloAvance.find().populate('proyecto').populate('creadoPor');
+            return avances;
+
+        },
+    },
+    Mutation: {
+        crearAvance: async (parent, args)=>{
+            const avanceCreado = ModeloAvance.create({
+                fecha: args.fecha,
+                descripcion: args.descripcion,
+                proyecto: args.proyecto,
+                creadoPor: args.creadoPor,
+            });
+            return avanceCreado;
+        },
+        filtrarAvance: async(parent, args)=>{
+            const avanceFiltrado = await ModeloAvance.find({ proyecto: args.idProyecto})
+                .populate("proyecto")
+                .populate(!"creadoPor");
+            return avanceFiltrado;
+        },
+    },
+};
+
+export { resolversAvance }; 
